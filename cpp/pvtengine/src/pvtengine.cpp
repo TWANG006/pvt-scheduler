f52134cb2eb2ec25de1b56221c1de475d4118942
Tianyi <tianyiwang666@gmail.com>
2022-07-05 13:31:01 -0400

    added Eigen & qpOASES supports

--- cpp/pvtengine/src/pvtengine.cpp
+++ cpp/pvtengine/src/pvtengine.cpp
@@ -4,7 +4,7 @@
 #include "pch.h"
 #include "framework.h"
 #include "pvtengine.h"
-
+#include <Eigen/Dense>
 
 // This is an example of an exported variable
 PVTENGINE_API int npvtengine = 0;
@@ -12,7 +12,13 @@ PVTENGINE_API int npvtengine = 0;
 // This is an example of an exported function.
 PVTENGINE_API int fnpvtengine(void)
 {
-	std::cout << "Hello world from PVT engine." << std::endl;
+	Eigen::MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
+
 	return 0;
 }
 
