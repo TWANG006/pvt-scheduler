724e93d1796312225782388879e27d42713ca2a3
Tianyi Wang <tianyiwang666@gmail.com>
2022-07-17 14:05:39 -0400

    simulator api done

--- cpp/pvtengine/include/Simulator.h
+++ cpp/pvtengine/include/Simulator.h
@@ -2,6 +2,7 @@
 #define SIMULATOR_H
 
 #include "pvtengine.h"
+#include "Interpolator.h"
 
 //! This class performs the velocity-based fabrication simulation.
 /*!
@@ -14,10 +15,44 @@
 * 6) With the up-sampled PVT and the 'Interpolator', calculate the
 *    removal at different dwell point using 'Simulator'
 */
-class Simulator
+class PVTENGINE_API Simulator
 {
 public:
+	//! No default constructor and copying
+	Simulator() = delete;
+	Simulator(const Simulator&) = delete;
+	Simulator& operator=(const Simulator&) = delete;
 
+	//! Construct the Simulator using the PVT and TIF
+	/*!
+	* This constructor builds the Simulator by precomputing the Bi-
+	* cubic interplation LUT using the TIF. This precomputed LUT
+	* can then be used for different PVT combinations.
+	*/
+	Simulator(
+		const MatrixXXd& Xtif,/*!< [in] TIF X grid*/
+		const MatrixXXd& Ytif,/*!< [in] TIF Y grid*/
+		const MatrixXXd& Ztif,/*!< [in] TIF Z grid*/
+		const MatrixXXd& X,   /*!< [in] Surface X grid*/
+		const MatrixXXd& Y,   /*!< [in] Surface Y grid*/
+		const MatrixXXd& Z    /*!< [in] Surface height*/
+	);
+
+	//! Overloaded () operator to calculate the estimated removal
+	/*!
+	* Simulating the removal based on the calculate PVTs
+	* /Return the estimated height removal
+	*/
+	MatrixXXd operator()(
+		const PVA& xPV,/*!< [in] PVT in x axis*/
+		const PVA& yPV /*!< [in] PVT in y axis*/
+	);
+
+private:
+	Interpolator m_interp;/*!< bicubic interpolator*/
+	MatrixXXd m_X;        /*!< surface X grid*/
+	MatrixXXd m_Y;        /*!< surface Y grid*/
+	MatrixXXd m_Z;        /*!< surface height*/
 };
 
 
--- cpp/pvtengine/src/Simulator.cpp
+++ cpp/pvtengine/src/Simulator.cpp
@@ -1,2 +1,15 @@
 #include "pch.h"
 #include "Simulator.h"
+
+Simulator::Simulator(const MatrixXXd& Xtif, const MatrixXXd& Ytif, const MatrixXXd& Ztif, const MatrixXXd& X, const MatrixXXd& Y, const MatrixXXd& Z)
+	: m_interp(Xtif, Ytif, Ztif)
+	, m_X(X)
+	, m_Y(Y)
+	, m_Z(Z)
+{
+}
+
+MatrixXXd Simulator::operator()(const PVA& xPV, const PVA& yPV)
+{
+	return MatrixXXd();
+}
