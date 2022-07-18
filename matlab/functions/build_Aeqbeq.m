aca03f003700063c4864213e7a5c68bef714a6ce
Tianyi Wang <tianyiwang666@gmail.com>
2022-07-17 21:57:13 -0400

    Update build_Aeqbeq.m

--- matlab/functions/build_Aeqbeq.m
+++ matlab/functions/build_Aeqbeq.m
@@ -10,8 +10,8 @@ function [Aeq, beq] = build_Aeqbeq(...
 
 n = length(pvt_t) - 1;
 % Aeq = zeros(n - 2, 6 * n);
-Aeq = zeros(2*(n - 2), 6 * n);
-beq = zeros(2*(n - 2), 1);
+Aeq = zeros(1*(n - 2), 6 * n);
+beq = zeros(1*(n - 2), 1);
 
 for j = 2: n - 1
     i = j + 1;
@@ -27,12 +27,12 @@ for j = 2: n - 1
     Aeq(j, id - 4) = 2;
     Aeq(j, id + 2) = -2;
 
-    Aeq(j - 1, id - 5) = 3 * pvt_t(i - 1)^2;
-    Aeq(j - 1, id + 1) = -3 * pvt_t(i - 1)^2;
-    Aeq(j - 1, id - 4) = 2 * pvt_t(i - 1);
-    Aeq(j - 1, id + 2) = -2 * pvt_t(i - 1);
-    Aeq(j - 1, id - 3) = 1;
-    Aeq(j - 1, id + 3) = -1;
+%     Aeq(j - 1, id - 5) = 3 * pvt_t(i - 1)^2;
+%     Aeq(j - 1, id + 1) = -3 * pvt_t(i - 1)^2;
+%     Aeq(j - 1, id - 4) = 2 * pvt_t(i - 1);
+%     Aeq(j - 1, id + 2) = -2 * pvt_t(i - 1);
+%     Aeq(j - 1, id - 3) = 1;
+%     Aeq(j - 1, id + 3) = -1;
 end
 
 
