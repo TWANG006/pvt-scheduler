d0b6291138933deb967eda971376ce497f09bd43
XLKE666 <85376795+XLKE666@users.noreply.github.com>
2022-08-01 20:32:56 +0800

    Update step_01_dt_udo_ibf.m

--- matlab/scripts/step_01_dt_udo_ibf.m
+++ matlab/scripts/step_01_dt_udo_ibf.m
@@ -19,28 +19,38 @@ load([dataDir pathFile]);
 surf_mpp = median(diff(X_P(1, :)));
 
 %% path 1
-% dwell points
-dwell_x = dp_x * surf_mpp;
-dwell_y = dp_y * surf_mpp;
-dwell_x = dwell_x + min(min(X_P));
-dwell_y = dwell_y + min(min(Y_P));
-% path points
-path_x = xp * surf_mpp;
-path_y = yp * surf_mpp;
-path_x = path_x + min(min(X_P));
-path_y = path_y + min(min(Y_P));
-
-%% path 2
 % % dwell points
-% dwell_x = xp * surf_mpp;
-% dwell_y = yp * surf_mpp;
+% dwell_x = dp_x * surf_mpp;
+% dwell_y = dp_y * surf_mpp;
 % dwell_x = dwell_x + min(min(X_P));
 % dwell_y = dwell_y + min(min(Y_P));
 % % path points
-% path_x = dp_x * surf_mpp;
-% path_y = dp_y * surf_mpp;
+% path_x = xp * surf_mpp;
+% path_y = yp * surf_mpp;
 % path_x = path_x + min(min(X_P));
 % path_y = path_y + min(min(Y_P));
+
+%% path 2
+% dwell points
+dwell_x = xp * surf_mpp;
+dwell_y = yp * surf_mpp;
+dwell_x = dwell_x + min(min(X_P));
+dwell_y = dwell_y + min(min(Y_P));
+% path points
+path_x = dp_x * surf_mpp;
+path_y = dp_y * surf_mpp;
+path_x = path_x + min(min(X_P));
+path_y = path_y + min(min(Y_P));
+
+% Smooth the corners of the maze path
+dwell_x1 = dwell_x;
+dwell_y1 = dwell_y;
+for i = 2: size(dwell_x1, 2)-1
+    if (dwell_x1(i) == dwell_x1(i-1) && dwell_y1(i) == dwell_y1(i+1)) || (dwell_x1(i) == dwell_x1(i+1) && dwell_y1(i) == dwell_y1(i-1))
+        dwell_x(i) = 0.5*(path_x(i-1) + path_x(i));
+        dwell_y(i) = 0.5*(path_y(i-1) + path_y(i));
+    end        
+end
 
 % figure;
 % plot(dwell_x, dwell_y, 'r-o');axis xy tight equal;
