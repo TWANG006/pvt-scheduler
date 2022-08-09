940a29a67efde9a29fb574858687b675b7e3cb40
Tianyi <tianyiwang666@gmail.com>
2022-08-09 08:29:18 -0400

    minor updates

--- matlab/functions/velocities_with_const_acc_sampler.m
+++ matlab/functions/velocities_with_const_acc_sampler.m
@@ -59,26 +59,16 @@ for n = 1: length(p) - 1
         t02ti = t02t1(1: i - 1);  % acc/dcc part
         ti2t1 = t02t1(i: end  );  % const a part
         
-        % handle the case where tau overlapped with both parts
-        t2 = t02t1(i) - ta(n);     % const a initial time
-        
         % calculation
         v02vi = v(n) + an(n) * (t02ti(:) - t(n));
         p02pi = p(n) + 0.5 * (v02vi + v(n)) .* (t02ti - t(n));
         a02ai = an(n);
         sa = (v(n + 1).^2 - v(n).^2) / (2 * an(n));
         
-        vi = v(n + 1);
-        pi = p(n) + sa + t2 * vi;
-        ai = 0;
-        
         vi2v1 = v(n + 1) * ones(length(ti2t1), 1);
         pi2p1 = p(n) + sa + vi2v1 .* (ti2t1 - ta(n));
         ai2a1 = 0;
         
-%         v02v1 = [v02vi(:); vi; vi2v1(:)];
-%         p02p1 = [p02pi(:); pi; pi2p1(:)];
-%         a02a1 = [a02ai(:); ai; ai2a1(:)]; 
         v02v1 = [v02vi(:); vi2v1(:)];
         p02p1 = [p02pi(:); pi2p1(:)];
         a02a1 = [a02ai(:); ai2a1(:)];
@@ -88,33 +78,6 @@ for n = 1: length(p) - 1
     vs = [vs; v02v1(:)];
     as = [as; a02a1(:)];
     
-%     % calculation for the acc/dcc segment
-%     if ~isempty(t02ti)
-%         v02vi = v(n) + an(n) * (t02ti - t(n));
-%         p02pi = p(n) + 0.5 * (v02vi + v(n)) .* (t02ti - t(n));
-%         a02ai = an(n);
-%         sa = (v(n + 1).^2 - v(n).^2) / (2 * a);
-%         
-%         ps = [ps; p02pi(:)];
-%         vs = [vs; v02vi(:)];
-%         as = [as; a02ai(:)];
-%         ts = [ts; t02ti(:)];
-%     else
-%         sa = 0;
-%     end
-%     
-%     
-%     % calculation for the const v segment
-%     if ~isempty(ti2t1)
-%         v02v1 = v(n + 1) * ones(size(ti2t1));
-%         p02p1 = p(n) + sa + v02v1.*(ti2t1 - ta(n));
-%         a02a1 = 0;
-%         
-%         ps = [ps; p02p1(:)];
-%         vs = [vs; v02v1(:)];
-%         as = [as; a02a1(:)];
-%         ts = [ts; ti2t1(:)];
-%     end
 end
 
 end
