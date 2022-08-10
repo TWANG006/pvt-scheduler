cf4f922eaf1f9b99d5b0450d9d98b12a43f7f13f
Tianyi <tianyiwang666@gmail.com>
2022-08-10 11:13:54 -0400

    Create pvt_sampler.m

---
+++ matlab/functions/pvt_sampler.m
@@ -0,0 +1,17 @@
+function [ps, vs, ts, as] = pvt_sampler(...
+    c, ... pvt coefficients
+    t, ... time points
+    tau... delta t to perform the simulation
+)
+% @purpose
+%   using the `tau` to up-sample the PVT grid to prepare for the simulation
+% @return
+%   ps: sampled potision
+%   vs: sampled velocities
+%   ts: sampled t
+%   as: sampled acc/dcc
+
+
+
+
+end
